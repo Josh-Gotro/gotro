78261075d96f562866db4e3ebe6c94bae2b2ca40
Joshua Gauthreaux <gotro@Joshuas-Mac-mini.local>
2024-01-04 14:16:21 -0900

    add some photos

--- src/pages/Glass/Glass.jsx
+++ src/pages/Glass/Glass.jsx
@@ -5,11 +5,15 @@ import image3 from '../../assets/cacti-trio-wip.JPG';
 import image4 from '../../assets/cacti-trio-wip2.JPG';
 import image5 from '../../assets/chess-wip.JPG';
 import image6 from '../../assets/first-cactus.JPG';
+import image7 from '../../assets/lightning.JPG';
+import image8 from '../../assets/bear.JPG';
+import image9 from '../../assets/skull.JPG';
+
 import './glass.css';
 
 
 const Glass = () => {
-  const images = [image1, image2, image3, image4, image5, image6];
+  const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const nextImage = () => {

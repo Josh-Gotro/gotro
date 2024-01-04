cc85e1a5769276b75f5db9f94b814b87682cc2e8
Joshua Gauthreaux <gotro@Joshuas-Mac-mini.local>
2024-01-03 19:57:53 -0900

    add more info to plaster calculator. adjust some styling

--- src/pages/PlasterCalculator.jsx
+++ src/pages/PlasterCalculator.jsx
@@ -6,8 +6,23 @@ const PlasterCalculator = () => {
   const [width, setWidth] = useState('');
   const [height, setHeight] = useState('');
   const [results, setResults] = useState(null);
+  const [showFormula, setShowFormula] = useState(false);
+  const [showCommonRatios, setShowCommonRatios] = useState(false);
+  const [showTiming, setShowTiming] = useState(false);
 
-const calculatePlasterAndWater = (length, width, height) => {
+  const handleFormulaToggle = () => {
+    setShowFormula(!showFormula);
+  };
+
+  const handleCommonRatiosToggle = () => {
+    setShowCommonRatios(!showCommonRatios);
+  };
+
+  const handleTimingToggle = () => {
+    setShowTiming(!showTiming);
+  };
+
+  const calculatePlasterAndWater = (length, width, height) => {
     const volume = length * width * height; // Volume of the object in cubic inches
     const volumeWithExtra = volume * 1.075; // Add 7.5% more volume to allow for spills, leaks, etc.
     const quartsWater = volumeWithExtra / 80; // Amount of water needed in quarts
@@ -18,15 +33,19 @@ const calculatePlasterAndWater = (length, width, height) => {
     const plasterOunces = (plasterWeightInPounds - plasterPounds) * 16; // Convert fractional pounds to ounces
 
     return {
-        volume: volumeWithExtra,
-        water: quartsWater * 32, // Convert quarts of water to ounces
-        plasterPounds,
-        plasterOunces
+      volume: volumeWithExtra,
+      water: quartsWater * 32, // Convert quarts of water to ounces
+      plasterPounds,
+      plasterOunces,
     };
-};
+  };
 
   const handleSubmit = () => {
-    const calcResults = calculatePlasterAndWater(Number(length), Number(width), Number(height));
+    const calcResults = calculatePlasterAndWater(
+      Number(length),
+      Number(width),
+      Number(height)
+    );
     setResults(calcResults);
   };
 
@@ -34,33 +53,218 @@ const calculatePlasterAndWater = (length, width, height) => {
     <div>
       <h2>Plaster Calculator</h2>
       <div className="input-group">
-      <input type="number" value={length} onChange={e => setLength(e.target.value)} placeholder="Length (inches)" />
-      <input type="number" value={width} onChange={e => setWidth(e.target.value)} placeholder="Width (inches)" />
-      <input type="number" value={height} onChange={e => setHeight(e.target.value)} placeholder="Height (inches)" />
-      <button onClick={handleSubmit}>Calculate</button>
+        <input
+          type="number"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+          placeholder="Length (inches)"
+        />
+        <input
+          type="number"
+          value={width}
+          onChange={(e) => setWidth(e.target.value)}
+          placeholder="Width (inches)"
+        />
+        <input
+          type="number"
+          value={height}
+          onChange={(e) => setHeight(e.target.value)}
+          placeholder="Height (inches)"
+        />
+        <button onClick={handleSubmit}>Calculate</button>
       </div>
-
 
       {results && (
         <table>
           <thead>
-              <tr>
-                  <th>LxWxH, inches</th>
-                  <th>Volume (cu in)</th>
-                  <th>Water required (oz)</th>
-                  <th>Plaster  required(lbs & oz)</th>
-              </tr>
+            <tr>
+              <th>LxWxH, inches</th>
+              <th>Volume (cu in)</th>
+              <th>Water required (oz)</th>
+              <th>Plaster required(lbs & oz)</th>
+            </tr>
           </thead>
           <tbody>
-              <tr>
-                  <td>{length} x {width} x {height}</td>
-                  <td>{results.volume.toFixed(2)}</td>
-                  <td>{results.water.toFixed(2)}</td>
-                  <td>{results.plasterPounds} lbs {results.plasterOunces.toFixed(2)} oz</td>
-              </tr>
+            <tr>
+              <td>
+                {length} x {width} x {height}
+              </td>
+              <td>{results.volume.toFixed(2)}</td>
+              <td>{results.water.toFixed(2)}</td>
+              <td>
+                {results.plasterPounds} lbs {results.plasterOunces.toFixed(2)}{' '}
+                oz
+              </td>
+            </tr>
           </tbody>
         </table>
       )}
+
+      <div className="show-more-info-section">
+        <span style={{ color: 'gold' }} onClick={handleFormulaToggle}>
+          {showFormula ? 'Hide formula' : 'see formula used to calculate'}
+        </span>
+        {showFormula && (
+          <div className="show-more-info">
+            <p>Rectangle Volume = length x width x height</p>
+            <p>2.85 lbs plaster : 1 quart (2 pounds) water.</p>
+            <p> makes 80 cubic inches of mixed plaster.</p>
+          </div>
+        )}
+
+        <span style={{ color: 'gold' }} onClick={handleCommonRatiosToggle}>
+          {showCommonRatios
+            ? 'Hide common mix ratios'
+            : 'see common mix ratios'}
+        </span>
+        {showCommonRatios && (
+          <table>
+            <thead>
+              <tr>
+                <th>WATER (quarts)</th>
+                <th>PLASTER Wt. (70)</th>
+                <th>PLASTER Wt. (66)</th>
+                <th>VOLUME CREATED (cu. in.)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>0.5</td>
+                <td>1.40</td>
+                <td>1.5</td>
+                <td>40</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>2.85</td>
+                <td>3</td>
+                <td>80</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>5.70</td>
+                <td>6</td>
+                <td>160</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>8.55</td>
+                <td>9</td>
+                <td>240</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>11.40</td>
+                <td>12</td>
+                <td>320</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>14.25</td>
+                <td>15</td>
+                <td>400</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>17.10</td>
+                <td>18</td>
+                <td>480</td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td>19.95</td>
+                <td>21</td>
+                <td>560</td>
+              </tr>
+              <tr>
+                <td>8</td>
+                <td>22.80</td>
+                <td>24</td>
+                <td>640</td>
+              </tr>
+              <tr>
+                <td>9</td>
+                <td>25.65</td>
+                <td>27</td>
+                <td>720</td>
+              </tr>
+              <tr>
+                <td>10</td>
+                <td>28.50</td>
+                <td>30</td>
+                <td>800</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
+
+        <span style={{ color: 'gold' }} onClick={handleTimingToggle}>
+          {showTiming ? 'Hide timing' : 'see timing'}
+        </span>
+        {showTiming && (
+          <>
+            <table>
+              <thead>
+                <tr>
+                  <th>Step description</th>
+                  <th>Add plaster</th>
+                  <th>Soak</th>
+                  <th>Mix</th>
+                  <th>Liquid</th>
+                  <th>Thix‐otropic</th>
+                  <th>Plastic</th>
+                  <th>Done</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Time per step (minute)</td>
+                  <td>1 </td>
+                  <td>3</td>
+                  <td>3</td>
+                  <td>3</td>
+                  <td>2</td>
+                  <td>3</td>
+                  <td>0</td>
+                </tr>
+                <tr>
+                  <td>Total time elapsed (minute)</td>
+                  <td>1 </td>
+                  <td>4</td>
+                  <td>7</td>
+                  <td>10</td>
+                  <td>12</td>
+                  <td>15</td>
+                  <td>15</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <table>
+              <thead>
+                <tr>
+                  <th>Plaster State</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Liquid plaster</td>
+                  <td>Flows like heavy cream</td>
+                </tr>
+                <tr>
+                  <td>Thixotropic plaster</td>
+                  <td>Stands on its own but returns to liquid when shaken</td>
+                </tr>
+                <tr>
+                  <td>Plastic plaster</td>
+                  <td>Has the ability to be modeled like clay</td>
+                </tr>
+              </tbody>
+            </table>
+          </>
+        )}
+      </div>
     </div>
   );
 };
